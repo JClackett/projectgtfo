8615e34cc8db5310a32c26da02bdd532c82b03e4
Jack Clackett <jclackett@gmail.com>
2016-01-24 20:09:46 +0000

    more setting up and updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!
+	before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # GET : Index
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,102 +4,102 @@ class TagsController < ApplicationController
 # CALLBACKS  
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  before_action :set_tag, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+	before_action :set_tag, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy, :show]
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # GET : Index
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def index
-    @tags = Tag.all
-  end
+	def index
+		@tags = Tag.all
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # GET : Show
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def show
-  end
+	def show
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # GET : New
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def new
-    @tag = Tag.new
-  end
+	def new
+		@tag = Tag.new
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # GET : Edit
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def edit
-  end
+	def edit
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
-# tag : Create
+# POST : Create
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def create
+	def create
 
-  @tag = Tag.new(tag_params) 
+		@tag = Tag.new(tag_params) 
 
-  respond_to do |format|
+		respond_to do |format|
 
-    if @tag.save
-      format.html { redirect_to @tag, notice: 'tag was successfully created.' }
-      format.json { render :show, status: :created, location: @tag }
-    else
-      format.html { render :new }
-      format.json { render json: @tag.errors, status: :unprocessable_entity }
-    end
+		  if @tag.save
+		    format.html { redirect_to @tag, notice: 'tag was successfully created.' }
+		    format.json { render :show, status: :created, location: @tag }
+		  else
+		    format.html { render :new }
+		    format.json { render json: @tag.errors, status: :unprocessable_entity }
+		  end
 
-  end
+		end
 
-  end
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # PUT : Update
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def update
-  respond_to do |format|
-    if @tag.update(tag_params)
-      format.html { redirect_to @tag, notice: 'tag was successfully updated.' }
-      format.json { render :show, status: :ok, location: @tag }
-    else
-      format.html { render :edit }
-      format.json { render json: @tag.errors, status: :unprocessable_entity }
-    end
-  end
-  end
+	def update
+		respond_to do |format|
+		  if @tag.update(tag_params)
+		    format.html { redirect_to @tag, notice: 'tag was successfully updated.' }
+		    format.json { render :show, status: :ok, location: @tag }
+		  else
+		    format.html { render :edit }
+		    format.json { render json: @tag.errors, status: :unprocessable_entity }
+		  end
+		end
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # DELETE : Destroy
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def destroy
-  @tag.destroy
-  respond_to do |format|
-    format.html { redirect_to tags_url, notice: 'tag was successfully destroyed.' }
-    format.json { head :no_content }
-  end
-  end
+	def destroy
+		@tag.destroy
+		respond_to do |format|
+		  format.html { redirect_to tags_url, notice: 'tag was successfully destroyed.' }
+		  format.json { head :no_content }
+		end
+	end
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # PRIVATE 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_tag
-    @tag = Tag.find(params[:id])
-  end
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_tag
+		@tag = Tag.find(params[:id])
+	end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def tag_params
-    params.require(:tag).permit(:title)
-  end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def tag_params
+		params.require(:tag).permit(:title)
+	end
 
 end
