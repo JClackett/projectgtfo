f6dcec84c35e8f1d0b69a259992a5c9ad00c1f0e
Jack Clackett <jclackett@gmail.com>
2016-01-27 14:12:47 +0000

    styling for timeline re done

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+
+	def timeline_datetimes(date) 
+		if date.today?
+			"Today"
+		elsif date.to_date == Date.yesterday
+			"Yesterday"
+		elsif (date > Date.today - 7) && (date < Date.yesterday)
+			date.strftime("%a")
+		else
+			date.strftime("%e %h")
+		end
+	end
 end
