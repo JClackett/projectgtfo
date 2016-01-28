9952b57811d741c329988ace80d092089a036454
Jack Clackett <jclackett@gmail.com>
2016-01-28 22:39:45 +0000

    big responsiveness stuff plus some styling

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -15,9 +15,9 @@ module PostsHelper
 	def embed(video)
 		unless video.blank?
 			youtube_id = video.split("=").last
-			content_tag(:iframe, nil, class: "video", src: "http://www.youtube.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0 autoplay=0" , allowfullscreen:0)
+			content_tag(:iframe, nil, class: "video", src: "http://www.youtube.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp; autoplay=0" , allowfullscreen:0)
   		end
   	end
-  	
+
 end
 
