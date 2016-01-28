663bfbfa82a9fba5329df603e10e17d1d75b4f18
George Borg <georgeborg00@hotmail.com>
2016-01-28 19:57:13 +0000

    video adding

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,4 +11,11 @@ module PostsHelper
 			date.strftime("%e %h")
 		end
 	end
+
+	def embed(video)
+		unless video.blank?
+			youtube_id = video.split("=").last
+			content_tag(:iframe, nil, class: "video", src: "http://www.youtube.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0 autoplay=0" , allowfullscreen:0)
+  		end
+  	end
 end
